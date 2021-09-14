6b46212328386e8de003a48fd0845d96f30cd861
James Thatcher <james@jameswt.com>
2021-09-14 21:14:16 +0100

    day8 part 2 done

--- src/Day8.php
+++ src/Day8.php
@@ -57,10 +57,68 @@ class Day8 extends DayBehaviour implements DayInterface
         return $accumulator;
     }
 
+    /**
+     * Fix the program so that it terminates normally by changing exactly one jmp (to nop) or nop (to jmp).
+     * What is the value of the accumulator after the program terminates?
+     *
+     * @return int|null
+     */
     public function solvePart2(): ?int
     {
-        // TODO: Implement solvePart2() method.
+        $input       = $this->getInputAsArray();
+        $accumulator = 0;
+        $insPointer  = 0;
 
-        return null;
+        // get a list of all nop and jmp instructions, preserving their index
+        // if we hit an infinite loop we'll pop one of these of and swap the instruction around
+        $nopAndJmp = array_filter($input, static fn ($i) => in_array($i['ins'], ['nop', 'jmp']));
+
+        $regression = null;
+        while (true) {
+            $i = &$input[$insPointer];
+
+            // natural finishing program, terminate and return accumulator
+            if (null === $i) {
+                break;
+            }
+
+            // we've entered an infinite loop, enter regression mode
+            // swap the next jmp/nop instruction from the stack
+            if (isset($i['done'])) {
+                if (($pointer = array_key_last($nopAndJmp)) === null) {
+                    // we've exhausted all options
+                    break;
+                }
+                array_walk($input, static function (&$i): void { unset($i['done']); }); // reset
+                $regression            = array_pop($nopAndJmp);
+                $regression['pointer'] = $pointer;
+                $insPointer            = 0;
+                $accumulator           = 0;
+                continue;
+            }
+
+            /** @noinspection NestedTernaryOperatorInspection */
+            // override our instruction if we're in regression
+            $ins = ($regression['pointer'] ?? null) === $insPointer
+                ? ('nop' === $regression['ins'] ? 'jmp' : 'nop')
+                : $i['ins'];
+
+            switch ($ins) {
+                case 'nop':
+                    $insPointer++;
+                    break;
+                case 'acc':
+                    $accumulator += $i['val'];
+                    ++$insPointer;
+                    break;
+                case 'jmp':
+                    $insPointer += $i['val'];
+                    break;
+            }
+
+            $i['done'] = true;
+        }
+
+        return $accumulator;
     }
 }
--- tests/Day8Test.php
+++ tests/Day8Test.php
@@ -15,7 +15,7 @@ test('solves part1')
     ->toBe(1753)
 ;
 
-test('solves part2')->skip()
+test('solves part2')
     ->expect(fn () => $this->day->solvePart2())
-    ->toBe(1250)
+    ->toBe(733)
 ;
