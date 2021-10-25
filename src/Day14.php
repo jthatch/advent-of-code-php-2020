2172da1f6cba61f601405d6eff17ba922148b261
James Thatcher <james@jameswt.com>
2021-10-25 20:03:15 +0100

    day 14 part 2

--- src/Day14.php
+++ src/Day14.php
@@ -15,18 +15,9 @@ class Day14 extends DayBehaviour implements DayInterface
 
     public function solvePart1(): int|string|null
     {
-        /*$this->input = explode("\n", <<<INPUT
-        mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
-               1100110010010100111001110110
-        mem[8] = 11
-        mem[7] = 101
-        mem[8] = 0
-        INPUT);
-        */
-        $input = $this->input;
-        $mem   = [];
-        $mask  = [];
-        foreach ($input as $i) {
+        $mem  = [];
+        $mask = [];
+        foreach ($this->input as $i) {
             // check for mask first
             if (1 === preg_match(static::REGEX['mask'], $i, $matches)) {
                 // split the mask into an array
@@ -37,7 +28,7 @@ class Day14 extends DayBehaviour implements DayInterface
             // a memory call
             if (1 === preg_match(static::REGEX['mem'], $i, $matches)) {
                 [$pos, $val] = [(int) $matches[1], (int) $matches[2]];
-                // convert value to binary and store in array
+                // convert value to binary
                 $valBin = sprintf('%036b', $val);
                 // now apply mask to $val
                 foreach ($mask as $offset => $m) {
@@ -56,6 +47,70 @@ class Day14 extends DayBehaviour implements DayInterface
 
     public function solvePart2(): int|string|null
     {
-        return null;
+        $mem  = [];
+        $mask = [];
+        foreach ($this->input as $i) {
+            // check for mask first
+            if (1 === preg_match(static::REGEX['mask'], $i, $matches)) {
+                // split the mask into an array
+                $mask = str_split($matches[1]);
+                continue;
+            }
+
+            // a memory call
+            if (1 === preg_match(static::REGEX['mem'], $i, $matches)) {
+                [$pos, $val] = [(int) $matches[1], (int) $matches[2]];
+                // convert position to binary
+                $posMask = sprintf('%036b', $pos);
+                // now apply mask to the position: 0 = unchanged, 1 = replace with 1, X = floating (see below)
+                foreach ($mask as $offset => $m) {
+                    if ('0' === $m) {
+                        continue;
+                    }
+                    $posMask[$offset] = $m;
+                }
+                // now our memory position in binary ($posBin) contains a number of floating values (X)
+                // these can be either 0 or 1 giving us 2^X possible memory combinations.
+
+                // Example: given a $posBin like 000000000000000000000000000000X1101X with 2 X's, we get 4 combinations:
+                //    000000000000000000000000000000(X)1101(X) becomes:
+                // 1. 000000000000000000000000000000(0)1101(0)
+                // 2. 000000000000000000000000000000(0)1101(1)
+                // 3. 000000000000000000000000000000(1)1101(0)
+                // 4. 000000000000000000000000000000(1)1101(1)
+
+                // with 3 it's 8 combinations.. and so on
+
+                // let's do this recursively
+                $posCombos = $this->maskTraverse($posMask);
+                foreach ($posCombos as $pos) {
+                    $mem[bindec($pos)] = $val;
+                }
+            }
+        }
+
+        return (int) array_sum($mem);
+    }
+
+    /**
+     * Recursively build up a list of all possible positions.
+     *
+     * @param string $posMask
+     *
+     * @return string[]
+     */
+    protected function maskTraverse(string $posMask): array
+    {
+        // get first occurrence of X in string
+        if (false === ($x = strpos($posMask, 'X', 0))) {
+            // if it doesn't exist that means we have a valid position, so return it as array
+            return [$posMask];
+        }
+
+        // otherwise, recursively call ourselves twice, for 0 and 1
+        return array_merge([], ...[
+            $this->maskTraverse(substr_replace($posMask, '0', $x, 1)),
+            $this->maskTraverse(substr_replace($posMask, '1', $x, 1)),
+        ]);
     }
 }
--- tests/Day14Test.php
+++ tests/Day14Test.php
@@ -12,7 +12,7 @@ uses()->beforeEach(function (): void {
 
 test('solves part1')
     ->expect(fn () => $this->day->solvePart1())
-    ->toBe(null)
+    ->toBe(14553106347726)
 ;
 
 test('solves part2')
