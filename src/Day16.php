1d08fc9efc6215e4ba23f151801d4d335f5601ef
James Thatcher <james@jameswt.com>
2021-10-30 16:05:36 +0100

    Day16 Part1 done

--- src/Day16.php
+++ src/Day16.php
@@ -8,15 +8,84 @@ use App\Interfaces\DayInterface;
 
 class Day16 extends DayBehaviour implements DayInterface
 {
-    public function solvePart1(): int|string|null
+    public function solvePart1(): ?int
     {
-        // TODO: Implement solvePart1() method.
-        return null;
+        /*$this->input = array_map(static fn(string $l) => $l . "\n", explode("\n",<<<INPUT
+        class: 1-3 or 5-7
+        row: 6-11 or 33-44
+        seat: 13-40 or 45-50
+
+        your ticket:
+        7,1,14
+
+        nearby tickets:
+        7,3,47
+        40,4,50
+        55,2,20
+        38,6,12
+        INPUT));*/
+
+        [$rules,, $nearby] = $this->getTicketDataFromInput($this->input);
+
+        $invalid = [];
+        foreach ($nearby as $ticket) {
+            foreach ($ticket as $n) {
+                $foundValidRule = false;
+                $ticketInvalid  = [];
+                foreach ($rules as [$a, $b]) {
+                    if (($n < $a[0] || $n > $a[1]) && ($n < $b[0] || $n > $b[1])) {
+                        $ticketInvalid[] = $n;
+                    } else {
+                        $foundValidRule = true;
+                    }
+                }
+                if (!$foundValidRule) {
+                    /** @noinspection SlowArrayOperationsInLoopInspection */
+                    $invalid = array_merge($invalid, array_unique($ticketInvalid));
+                }
+            }
+        }
+
+        return (int) array_sum($invalid);
     }
 
-    public function solvePart2(): int|string|null
+    public function solvePart2(): ?int
     {
         // TODO: Implement solvePart2() method.
         return null;
+    }
+
+    protected function getTicketDataFromInput(array $input): array
+    {
+        $ticket = [
+            [], // rules
+            [], // my
+            [], // nearby
+        ];
+        $inputPositions = array_keys($ticket);
+        // start the pointer at rules
+        $p = array_shift($inputPositions);
+        // loop over each input, when we encounter a blank new line, update our pointer ($p) to the next position
+        foreach ($input as $line) {
+            if (PHP_EOL === $line) {
+                $p = array_shift($inputPositions);
+            }
+            $line = trim($line);
+
+            // parse a rule e.g.: departure location: 29-458 or 484-956
+            if (0 === $p) {
+                [$ruleName, $numbers] = explode(':', $line, 2);
+                if (1 <= preg_match_all('/(\d+)/', $numbers, $matches)) {
+                    $ticket[$p][$ruleName] = array_map(static fn (array $a) => array_map('intval', $a), array_chunk($matches[1], 2));
+                }
+            } elseif (str_contains($line, ',')) { // parse the comma separated list of numbers
+                $nums = explode(',', $line);
+                if (!empty($nums)) {
+                    $ticket[$p][] = array_map('intval', $nums);
+                }
+            }
+        }
+
+        return $ticket;
     }
 }
---
+++ tests/Day16Test.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+use App\DayFactory;
+use App\Interfaces\DayInterface;
+
+uses()->beforeEach(function (): void {
+    /* @var DayInterface day */
+    $this->day = DayFactory::create(getDayFromFile(__FILE__));
+});
+
+test('solves part1')
+    ->expect(fn () => $this->day->solvePart1())
+    ->toBe(20048)
+;
+
+test('solves part2')
+    ->expect(fn () => $this->day->solvePart2())
+    ->toBe(null)
+;
