e89b76870094366e7cc8f34bd8fa466ac6134954
James Thatcher <james@jameswt.com>
2022-02-12 20:24:02 +0000

    day17 wip

---
+++ src/Day17.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use App\Interfaces\DayInterface;
+
+class Day17 extends DayBehaviour implements DayInterface
+{
+    public function solvePart1(): ?int
+    {
+        // TODO: Implement solvePart1() method.
+        return null;
+    }
+
+    public function solvePart2(): ?int
+    {
+        // TODO: Implement solvePart2() method.
+        return null;
+    }
+}
+
